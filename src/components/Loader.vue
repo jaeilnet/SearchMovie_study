44e7f168d1516a4d12a34d672e9701196457c305
jaeilnet <sgsgself@naver.com>
2021-07-14 01:21:03 +0900

    ~loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div 
+    :class="{ absolute, fixed }"
+    class="spinner-border text-primary"></div>  
+</template>
+
+<script>
+export default {
+  props:{
+    size:{
+      type: Number,
+      default: 2
+    },
+    absolute:{
+      type: Boolean,
+      default:false
+    },
+    fixed:{
+      type:Boolean,
+      default: false
+    },
+    zIndex:{
+      type:Number,
+      default:0
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHashHistory } from "vue-router"
+import Home from './Home'
+import About from './About'
+import Movie from './Movie'
+
+export default createRouter({
+  // Hash 모드
+  // https://google.com/#search 와 같이 #(hash)으로 접근
+  history: createWebHashHistory(),
+  // pages 구분
+  routes : [
+    {
+      path: '/',   // main을 의미
+      component: Home
+    },
+    {
+      path :'/movie/:id',
+      component: Movie
+    },
+    {
+      path: '/about',
+      component: About
+    }
+  ]
+})
